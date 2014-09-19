2cb47df32cd7fa1a953159d840e8ac1ec5dbe9ba
Shinobu Yasuoka <SHINOBU@Shinobu-Yasuokas-MacBook-Pro.local>
2014-09-18 21:39:36 -0700

    Using only 1 version of underscore (the one independent of hbs) using shim in require config.  Also using handlebars that's not included with hbs

--- src/requirejs-config.js
+++ src/requirejs-config.js
@@ -10,17 +10,22 @@
             lodash: 'bower_components/lodash/dist/lodash',
             tmpl: 'bower_components/lodash-template-loader/loader',
             hbs: 'bower_components/require-handlebars-plugin/hbs',
-            'hbs/underscore': 'bower_components/require-handlebars-plugin/hbs/underscore',
             'hbs/i18nprecompile': 'bower_components/require-handlebars-plugin/hbs/i18nprecompile',
             'hbs/json2': 'bower_components/require-handlebars-plugin/hbs/json2',
-            'hbs/handlebars': 'bower_components/require-handlebars-plugin/hbs/handlebars'
-
+            'handlebars': 'bower_components/handlebars'
         },
+
         hbs: {
             helpers: true,
             i18n: false,
             templateExtension: 'html',
             partialsUrl: ''
+        },
+
+        shim: {
+            underscore: {
+                exports: '_'
+            }
         },
 
         deps: ['main']
